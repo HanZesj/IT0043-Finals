26b7875525b81db54bff25d37fd6d6b534b9212c
HanZesj <169737687+HanZesj@users.noreply.github.com>
2024-07-12 07:46:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('#nav-tabs a');
+    const bannerText = document.querySelector('#banner-text');
+    const subText = document.querySelector('#sub-text');
+    const headerText = document.querySelector('.header-text-section');
+    const banner = document.querySelector('#banner');
+    const textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, a');
+
+    // Add animation to navigation links
+    navLinks.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            link.classList.add('hovered');
+        });
+        link.addEventListener('mouseout', () => {
+            link.classList.remove('hovered');
+        });
+    });
+
+    // Fade-in effect for banner text and sub text
+    window.addEventListener('load', () => {
+        bannerText.classList.add('fade-in');
+        subText.classList.add('fade-in');
+    });
+
+    // Slide-in effect for header text and background color change on scroll
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 50) {
+            headerText.classList.add('slide-in');
+        } else {
+            headerText.classList.remove('slide-in');
+        }
+
+        // Change body background color based on scroll position
+        document.body.style.backgroundColor = scrollPosition > 100 ? '#292929' : ''; // Example threshold
+    });
+
+    // Change #banner background on mouse hover
+    banner.addEventListener('mouseover', function() {
+        this.style.backgroundImage = 'linear-gradient(to right, #3e2b22, #191818)';
+    });
+
+    banner.addEventListener('mouseout', function() {
+        this.style.backgroundImage = 'url(\'/images/1.png\')';
+    });
+
+    // Enlarge #banner-text font size on click
+    bannerText.addEventListener('click', function() {
+        this.style.fontSize = '5em'; // Increase font size
+        // Ensure to reset to original size or toggle based on your preference
+    });
+
+    // Fade-in effect on text elements on mouseover
+    function addFadeInAnimation() {
+        this.classList.add('fade-in');
+    }
+
+    textElements.forEach(element => {
+        element.addEventListener('mouseover', addFadeInAnimation);
+    });
+});
