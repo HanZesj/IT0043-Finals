c6d55fbd746a8c35c63d0a6aadcb50e140934236
RAbellar13 <202311305@fit.edu.ph>
2024-07-12 10:02:26 +0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
